408fc14324c41de33bf03cb4c57d677c615d6b51
Hayreddin Tüzel <hayreddin.tuzel@osf-commerce.com>
2019-07-20 00:49:16 +0300

    Implemented middleware

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Exception;
+use App\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+
+use Closure;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->header('Authorization');
+
+        if(!$token) {
+
+            return response()->json([
+                'status' => 401,
+                'error' => 'Token required.'
+            ], 401);
+        }
+
+        try {
+            $credentials = JWT::decode($token, env('SECRET'), ['HS256']);
+        } catch(ExpiredException $e) {
+
+            return response()->json([
+                'error' => 'Provided token is expired.'
+            ], 400);
+        } catch(Exception $e) {
+            return response()->json([
+                'error' => 'An error while decoding token.'
+            ], 400);
+        }
+
+        $user = User::find($credentials->sub);
+
+        if(!empty($user)){
+            $request->auth = $user;
+        }else{
+
+            return response()->json([
+                'error' => 'Provided token is invalid.'
+            ], 400);
+        }
+
+        return $next($request);
+    }
+}
